c51cf30ec060e96711c9bf93ff45d15b7f132655
Vgr E.Barry <vgr255@live.ca>
2015-04-04 20:08:32 -0400

    Add brainfuck support
    The encode method was made by @skizzerz - I simply copied it from PHP

--- encoder.py
+++ encoder.py
@@ -8,7 +8,7 @@ method returns a string, uppercase for hashes.
 import hashlib as _hashlib
 import base64 as _base64
 
-__all__ = ["base64", "morse", "rot13"]
+__all__ = ["base64", "morse", "brainfuck", "rot13"]
 
 _rot13 = {}
 
@@ -75,6 +75,170 @@ class morse:
     @staticmethod
     def decode(line):
         return "".join((_morse_dec[c].lower() for c in line.split(" ")))
+
+class BFHandler:
+    def __init__(self):
+        self.handler = [0]
+        self.pointer = 0
+        self.brackets = {}
+
+    def open_bracket(self, pointer):
+        self.brackets[pointer] = None
+
+    def close_bracket(self, pointer):
+        toget = sorted(self.brackets)[-1]
+        self.brackets[toget] = pointer
+
+    def move_right(self):
+        self.pointer += 1
+        if len(self.handler) <= self.pointer:
+            self.handler.append(0)
+
+    def move_left(self):
+        self.pointer -= 1
+
+    def inc(self):
+        self.handler[self.pointer] += 1
+        if self.handler[self.pointer] == 256:
+            self.handler[self.pointer] = 0
+
+    def dec(self):
+        self.handler[self.pointer] -= 1
+        if self.handler[self.pointer] == -1:
+            self.handler[self.pointer] = 255
+
+    def get_value(self):
+        return chr(self.handler[self.pointer])
+
+    def set_value(self, inp):
+        self.handler[self.pointer] = ord(inp)
+
+    def get_current(self):
+        return self.handler[self.pointer]
+
+    def reverse_brackets(self):
+        return {v:k for k,v in self.brackets.items()}
+
+class brainfuck:
+
+    _allowed = "+-.,[]<>"
+
+    @staticmethod
+    def offset(run, items):
+        off = 0
+        if run == 0:
+            return 0
+        runner = 1
+        while runner < run:
+            if runner not in items:
+                off += 1
+            runner += 1
+        return off
+
+    @classmethod
+    def decode(cls, line):
+        new = ""
+        for char in line:
+            if char in cls._allowed:
+                new += char
+        if not new:
+            return ""
+        cells = BFHandler()
+        pointer = 0
+        for char in new:
+            if char == "[":
+                cells.open_bracket(pointer)
+            elif char == "]":
+                cells.close_bracket(pointer)
+            pointer += 1
+        pointer = 0
+        chars = ""
+        while True:
+            char = new[pointer]
+            if char == "+":
+                cells.inc()
+            elif char == "-":
+                cells.dec()
+            elif char == "[":
+                if not cells.get_current():
+                    pointer = cells.brackets[pointer]
+            elif char == "]":
+                if cells.get_current():
+                    pointer = cells.reverse_brackets()[pointer]
+            elif char == "<":
+                cells.move_left()
+            elif char == ">":
+                cells.move_right()
+            elif char == ".":
+                chars += cells.get_value()
+            elif char == ",":
+                print("Comma found.")
+                while True:
+                    c = input("Please enter a single character and hit Enter: ").strip()
+                    if len(c) == 1:
+                        break
+                    print("Too many characters, please try again.")
+                cells.set_value(c)
+            pointer += 1
+            if pointer == len(new):
+                break
+        return chars
+
+    @classmethod
+    def encode(cls, line, maxlen=10):
+        if maxlen < 1:
+            maxlen = 1
+        maxlen = maxlen // 1
+        runner = 0
+        #items = [0, (256 // maxlen), False]
+        items = {}
+        ords = []
+        new = ""
+        while runner < len(line):
+            ords.append(ord(line[runner]))
+            items[ord(line[runner]) // maxlen] = True
+            runner += 1
+        if maxlen <= max(ords) and maxlen > 1:
+            new += "+" * maxlen + "["
+        runner = 0
+        for key, value in items.items():
+            if key == 0 or not value:
+                continue
+            new += ">" + "+" * key
+            runner += 1
+        while runner > 0:
+            new += "<"
+            runner -= 1
+        if maxlen <= max(ords) and maxlen > 1:
+            new += "-]"
+        runner = 1
+        values = {}
+        while runner < (256 // maxlen) + 1:
+            offset = cls.offset(runner, items)
+            if runner > offset:
+                values[runner - offset] = maxlen * runner
+            runner += 1
+        runner = 0
+        run = 0
+        while runner < len(line):
+            r = ord(line[runner])
+            num = r // maxlen
+            offset = cls.offset(num, items)
+            num -= offset
+            val = values[num]
+            if num > run:
+                new += ">" * (num - run)
+            elif num < run:
+                new += "<" * (run - num)
+            run = num
+            if r > val:
+                new += "+" * (r - val)
+            elif r < val:
+                new += "-" * (val - r)
+            new += "."
+            values[num] = r
+            runner += 1
+        return new
 
 def rot13(line):
     return "".join((_rot13.get(c, c) for c in line))
